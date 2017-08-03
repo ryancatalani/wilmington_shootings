bfc1a59cf3f8c522c0f06d0c2644ca1b638d3eea
Ryan Catalani <catalani.ryan@gmail.com>
2017-08-03 14:05:01 -0500

    Add gulp task to combine styles also.

--- BUILD
+++
@@ -1,4 +0,0 @@
-cat src/styles/leaflet.css src/styles/dev.css > assets/main.css
-
-
-cat src/js/jquery.min.js src/js/leaflet.js src/js/topojson.v1.min.js src/js/papaparse.min.js src/js/moment.min.js src/js/Chart.min.js src/js/underscore-min.js src/js/dev2.js > assets/main.js
--- web/gulpfile.js
+++ web/gulpfile.js
@@ -6,7 +6,7 @@ var uglify = require('gulp-uglify');
 var jsFiles = ['src/js/libs/*.js', 'src/js/dev2.js'],
     jsDest = 'assets';
 
-gulp.task('default', function() {
+gulp.task('js', function() {
     return gulp.src(jsFiles)
         .pipe(concat('main.js'))
         .pipe(gulp.dest(jsDest))
@@ -14,3 +14,14 @@ gulp.task('default', function() {
         .pipe(uglify())
         .pipe(gulp.dest(jsDest));
 });
+
+var styleFiles = ['src/styles/leaflet.css', 'src/styles/dev.css'],
+	styleDest = 'assets';
+
+gulp.task('styles', function() {
+	return gulp.src(styleFiles)
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest(styleDest));
+})
+
+gulp.task('default', ['js', 'styles']);
